f31198c0a4289a6db0f40eb530c3f2f008193cb2
Suraj0791 <surajstudent9122@gmail.com>
2025-09-15 17:33:59 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
+
+import "./globals.css";
+import { ExitModal } from "@/components/modals/exit-modal";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Wordnest",
+  description: "Learn, practice, and master new languages with Linguify.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>
+          <Toaster />
+          <ExitModal />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+// next.config.mjs
+const nextConfig = {
+  async headers(){
+    return[
+      {
+        source:"/api/(.*)",
+        headers:[
+          { key:"Access-Control-Allow-Origin", value:"*" },
+          { key:"Access-Control-Allow-Methods", value:"GET,POST,PUT,OPTIONS,DELETE" },
+          { key:"Access-Control-Allow-Headers", value:"Content-type,Authorization" },
+          { key:"Content-Range", value:"bytes : 0-9/*" },
+        ]
+      }
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
