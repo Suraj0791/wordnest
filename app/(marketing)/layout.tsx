2927f31399c131c66b441da960846a87550f7248
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-15 18:56:59 +0530

    feat(marketing): build public landing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,20 @@
-interface MarketingLayoutProps {
-  children: React.ReactNode;
-}
+import { Header } from "./header";
+import { Footer } from "./footer";
 
-const MarketingLayout = ({ children }: MarketingLayoutProps) => {
-  return <div>{children}</div>;
+type Props = {
+  children: React.ReactNode;
 };
 
+const MarketingLayout = ({ children }: Props) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+ 
 export default MarketingLayout;
