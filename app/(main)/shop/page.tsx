d5580da0a3d12276c2961b96841cb944dcf63538
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-16 14:07:42 +0530

    feat: add stripe payment

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,18 +1,22 @@
 import FeedWrapper from "@/components/FeedWrapper";
 import StickyWrapper from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/UserProgress";
-import { getUserProgress } from "@/db/queries";
-import Image from "next/image";
+import { getUserProgress, getUserSubscriptions } from "@/db/queries";
 import { redirect } from "next/navigation";
 import ItemsPage from "./_components/items";
+import ShopJson from "./_components/lottie";
 
 const ShopPage = async() => {
     const userProgressData = getUserProgress();
-    const [userProgress] = await Promise.all([userProgressData])
+    const userSubscriptionData = getUserSubscriptions();
+    const [userProgress,userSubscription] = await Promise.all([userProgressData,userSubscriptionData])
 
     if(!userProgress || !userProgress.activeCourse){
         redirect("/courses");
     }
+
+    const isPro = !!userSubscription?.isActive
+
 
     return ( 
         <div className="flex flex-row-reverse gap-[42px] px-6">
@@ -21,20 +25,15 @@ const ShopPage = async() => {
             activeCourse={userProgress.activeCourse}
             hearts={userProgress.hearts}
             points={userProgress.points}
-            hasActiveSubscription={false}
+            hasActiveSubscription={isPro}
             />
             </StickyWrapper>
             <FeedWrapper
             >
                 <div className="w-full flex flex-col items-center">
-                <Image
-                src="/shop.svg"
-                alt="Shop"
-                height={100}
-                width={100}
-                />
+                <ShopJson/>
                 <h1 className="text-center font-bold text-neutral-800 dark:text-slate-200 text-2xl my-6">
-                    Shop
+                    Shop Here 🤩
                 </h1>
                 <p className="text-muted-foreground text-center text-lg mb-6">
                     Spend points on your cool stuff with fun.
@@ -42,7 +41,7 @@ const ShopPage = async() => {
                 <ItemsPage
                 hearts={userProgress.hearts}
                 points={userProgress.points}
-                hasActiveSubscription={false} //Todo: add subscription
+                hasActiveSubscription={isPro} 
                 />
                 </div>
             </FeedWrapper>
