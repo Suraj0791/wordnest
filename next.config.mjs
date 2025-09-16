43cc670c981fcd68227ab48b3dd44d098618d36a
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-16 22:04:04 +0530

    next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,47 @@
 // next.config.mjs
 
 const nextConfig = {
-  async headers(){
-    return[
+  async headers() {
+    return [
       {
-        source:"/api/(.*)",
-        headers:[
+        source: "/api/(.*)",
+        headers: [
           {
-            key:"Access-Control-Allow-Origin",
-            value:"*"
+            key: "Access-Control-Allow-Origin",
+            value: "*",
           },
           {
-            key:"Access-Control-Allow-Methods",
-            value:"GET,POST,PUT,OPTIONS,DELETE",
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,OPTIONS,DELETE",
           },
           {
-            key:"Access-Control-Allow-Headers",
-            value:"Content-type,Authorization",
+            key: "Access-Control-Allow-Headers",
+            value: "Content-type,Authorization",
           },
           {
-            key:"Content-Range",
-            value:"bytes : 0-9/*",
+            key: "Content-Range",
+            value: "bytes : 0-9/*",
           },
-        ]
-      }
+        ],
+      },
     ];
   },
-    images: {
-      remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'img.clerk.com',
-            port: '',
-            pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
