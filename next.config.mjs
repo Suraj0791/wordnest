dfd7c799b787df0cc3636d30aacc931327d69434
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-16 23:20:09 +0530

    feat : netlify toml

--- next.config.mjs
+++ next.config.mjs
@@ -1,53 +1,41 @@
 // next.config.mjs
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const nextConfig = {
-  async headers() {
-    return [
+  async headers(){
+    return[
       {
-        source: "/api/(.*)",
-        headers: [
+        source:"/api/(.*)",
+        headers:[
           {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
+            key:"Access-Control-Allow-Origin",
+            value:"*"
           },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,OPTIONS,DELETE",
+            key:"Access-Control-Allow-Methods",
+            value:"GET,POST,PUT,OPTIONS,DELETE",
           },
           {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-type,Authorization",
+            key:"Access-Control-Allow-Headers",
+            value:"Content-type,Authorization",
           },
           {
-            key: "Content-Range",
-            value: "bytes : 0-9/*",
+            key:"Content-Range",
+            value:"bytes : 0-9/*",
           },
-        ],
-      },
+        ]
+      }
     ];
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "."),
-    };
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'img.clerk.com',
+            port: '',
+            pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
