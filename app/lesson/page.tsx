674bb6df0aa3aac80d569991a45d551bca40658a
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-16 13:38:30 +0530

    feat: added challenge progress action to check ans.

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,37 +1,30 @@
-import { redirect } from "next/navigation";
-
 import { getLesson, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import Quiz from "./_components/Quiz";
 
-import { Quiz } from "./quiz";
+const LessonPage = async() => {
+    const lessonData =  getLesson();
+    const userProgressData =  getUserProgress();
 
-const LessonPage = async () => {
-  const lessonData = getLesson();
-  const userProgressData = getUserProgress();
+    const[lesson,userProgress] = await Promise.all([lessonData,userProgressData]);
 
-  const [
-    lesson,
-    userProgress,
-  ] = await Promise.all([
-    lessonData,
-    userProgressData,
-  ]);
+    if(!lesson || !userProgress) {
+        redirect("/learn");
+    }
 
-  if (!lesson || !userProgress) {
-    redirect("/learn");
-  }
+    const initialPercentage = lesson.challenges
+    .filter((challenge)=>challenge.completed)
+    .length / lesson.challenges.length*100;
 
-  const initialPercentage = lesson.challenges
-    .filter((challenge) => challenge.completed)
-    .length / lesson.challenges.length * 100;
-
-  return (
-    <Quiz
-      initialLessonId={lesson.id}
-      initialLessonChallenges={lesson.challenges}
-      initialHearts={userProgress.hearts}
-      initialPercentage={initialPercentage}
-    />
-  );
-};
+    return ( 
+        <Quiz
+        initialLessonId = {lesson.id}
+        initialLessonChallenges = {lesson.challenges}
+        initialHearts = {userProgress.hearts}
+        initialPercentage = {initialPercentage}
+        userSubscription= {null}
+        />
+    );
+}
 
 export default LessonPage;
